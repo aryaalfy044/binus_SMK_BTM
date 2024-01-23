b8853d3c51c9edb52cfb9eb040845a1d4b2068ff
aryaalfy044 <aryamax27@gmail.com>
2024-01-23 21:57:25 +0700

    login icon

--- resources/views/Login/login.blade.php
+++ resources/views/Login/login.blade.php
@@ -26,7 +26,7 @@
           <div class="col-lg-6 mx-auto">
             <div class="auth-form-light text-left py-5 px-4 px-sm-5">
               <div class="brand-logo">
-                <img src="{{ asset('admin/images/logo-dark.svg') }}" alt="logo">
+                <img src="{{ asset('images/logo.png') }}" alt="logo">
               </div>
               <h4>Hello! let's get started</h4>
               <h6 class="font-weight-light">Sign in to continue.</h6>
