efca24ec6cef9d011c96bbf69cf2d3461b0135ed
knightofcyndonia <knightofcyndonia@gmail.com>
2024-01-28 17:41:35 +0800

    task list report with datatable

--- resources/views/TaskList/task-list-report.blade.php
+++ resources/views/TaskList/task-list-report.blade.php
@@ -11,92 +11,104 @@
 </style>
 <div class="container">
     <h2>Task Report</h2>
-    <!-- Your home page content goes here -->
 
+    <div class="row">
+        <div class="col col-12 grid-margin">
+            <div class="card">
+                <div class="card-body">
+                    <form class="form-sample">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group row">
+                                    <label class="col-sm-3 col-form-label">From</label>
+                                    <div class="col-sm-9">
+                                        <input type="date" class="form-control" id="from" name="from" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group row">
+                                    <label class="col-sm-3 col-form-label">To</label>
+                                    <div class="col-sm-9">
+                                        <input type="date" class="form-control" id="to" name="to" />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-    <div class="col-12 grid-margin">
-        <div class="card">
-            <div class="card-body">
-                <form class="form-sample">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group row">
-                                <label class="col-sm-3 col-form-label">From</label>
-                                <div class="col-sm-9">
-                                    <input type="date" class="form-control" id="from" name="from" />
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group row">
+                                    <label class="col-sm-3 col-form-label">Employee Name</label>
+                                    <div class="col-sm-9">
+                                        <select class="form-control" id="employee">
+                                            <option selected value="0">All</option>
+                                            @foreach ($users as $u)
+                                                <option value="{{$u->id }}">{{$u->name}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group row">
+                                    <label class="col-sm-3 col-form-label"></label>
+                                    <div class="col-sm-9">
+                                        <button type="submit" class="btn btn-primary mb-2">Filter</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="form-group row">
-                                <label class="col-sm-3 col-form-label">To</label>
-                                <div class="col-sm-9">
-                                    <input type="date" class="form-control" id="to" name="to" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group row">
-                                <label class="col-sm-3 col-form-label">Employee Name</label>
-                                <div class="col-sm-9">
-                                    <select class="form-control" id="employee">
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group row">
-                                <label class="col-sm-3 col-form-label"></label>
-                                <div class="col-sm-9">
-                                    <button type="submit" class="btn btn-primary mb-2">Filter</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
-        </div>
 
-        <div class="card grid-margin">
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th style="width:30px!important">Employee Name</th>
-                                <th style="width:30px!important">Title</th>
-                                <th style="width:30px!important">Description</th>
-                                <th>Start Date</th>
-                                <th>End Date</th>
-                                <th>Progress</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @php $count = 1 @endphp
-                            @foreach ($tasks as $task)
-                            <tr>
-                                <td>{{ $count++ }}</td>
-                                <td>{{ $task->user_name }}</td>
-                                <td class="wrap-text">{{ $task->title }}</td>
-                                <td class="wrap-text">{{ $task->description }}</td>
-                                <td>{{ date('d/m/Y', strtotime($task->start_date)) }}</td>
-                                <td>{{ date('d/m/Y', strtotime($task->end_date)) }}</td>
-                                <td>{{ $task->progress }}</td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+        </div>
+        <div class="col col-12 grid-margin">
+            <div class="card grid-margin">
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover" id="tblReport">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th style="width:30px!important">Employee Name</th>
+                                    <th style="width:30px!important">Title</th>
+                                    <th style="width:30px!important">Description</th>
+                                    <th>Start Date</th>
+                                    <th>End Date</th>
+                                    <th>Progress</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php $count = 1 @endphp
+                                @foreach ($tasks as $task)
+                                <tr>
+                                    <td>{{ $count++ }}</td>
+                                    <td>{{ $task->user_name }}</td>
+                                    <td class="wrap-text">{{ $task->title }}</td>
+                                    <td class="wrap-text">{{ $task->description }}</td>
+                                    <td>{{ date('d/m/Y', strtotime($task->start_date)) }}</td>
+                                    <td>{{ date('d/m/Y', strtotime($task->end_date)) }}</td>
+                                    <td>{{ $task->progress }}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
+    <script>
+        $(document).ready(function () {
+            $('#tblReport').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'pdfHtml5'
+                ]
+            });
+        });
+    </script>
     @endsection
